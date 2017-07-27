76334d1aa9309df895b2be703b1739b8b5f80571
Patrick G <geek1011@outlook.com>
2017-07-27 17:13:37 -0400

    Add base command

--- kepubify.go
+++ kepubify.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"os"
 
+	cli "gopkg.in/urfave/cli.v1"
+)
+
+var version = "dev"
+
+func convert(c *cli.Context) error {
+	return nil
+}
+
+func main() {
+	app := cli.NewApp()
+
+	app.Name = "kepubify"
+	app.Description = "Convert your ePubs into kepubs, with a easy-to-use command-line tool."
+	app.Version = version
+
+	app.ArgsUsage = "EPUB_INPUT_PATH [KEPUB_OUTPUT_PATH]"
+	app.Action = convert
+
+	app.Run(os.Args)
 }
