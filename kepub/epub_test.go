a7b28c3a0860198e471774e534340d842da3d432
Patrick G <geek1011@outlook.com>
2017-07-28 12:06:52 -0400

    Added tests for kepub conversion and packing

--- kepub/epub_test.go
+++ kepub/epub_test.go
@@ -1,6 +1,10 @@
 package kepub
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,4 +18,21 @@ func TestExists(t *testing.T) {
 func TestIsDir(t *testing.T) {
 	assert.True(t, isDir("."), ". should be a dir")
 	assert.False(t, isDir("./epub_test.go"), "./epub_test.go should not be a dir")
+}
+
+func TestPackUnpack(t *testing.T) {
+	td, err := ioutil.TempDir("", "kepubify")
+	if err != nil {
+		assert.FailNow(t, fmt.Sprintf("%v", err), "Could not create temp dir")
+	}
+	defer os.RemoveAll(td)
+
+	assert.Nil(t, PackEPUB(filepath.Join("testdata", "books", "test1"), filepath.Join(td, "test1.epub"), true), "packepub should not return an error")
+	assert.True(t, exists(filepath.Join(td, "test1.epub")), "output epub should exist")
+
+	assert.Nil(t, UnpackEPUB(filepath.Join(td, "test1.epub"), filepath.Join(td, "test1"), true), "unpackepub should not return an error")
+	assert.True(t, exists(filepath.Join(td, "test1")), "output dir should exist")
+	assert.True(t, exists(filepath.Join(td, "test1", "META-INF", "container.xml")), "META-INF/container.xml should exist in output dir")
+
+	assert.NotNil(t, PackEPUB(filepath.Join("testdata", "books", "invalid"), filepath.Join(td, "invalid.epub"), true), "packepub should return an error for an epub withot a container.xml")
 }
