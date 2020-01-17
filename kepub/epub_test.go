fefa22c6145a59f9c6ab64691d1fe674048a2a32
Patrick Gaskin <patrick@pgaskin.net>
2020-01-17 16:47:52 -0500

    Added EPUB tests

--- kepub/epub_test.go
+++ kepub/epub_test.go
@@ -1,9 +1,15 @@
 package kepub
 
 import (
+	"archive/zip"
+	"crypto/sha256"
+	"encoding/binary"
+	"encoding/hex"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"sort"
+	"strings"
 	"testing"
 )
 
@@ -58,4 +64,114 @@ func TestFindOPF(t *testing.T) {
 	}
 }
 
-// TODO: test unpacking/packing (especially things like path separators in the packed epub)
+func TestEPUBUnpackPack(t *testing.T) {
+	if err := UnpackEPUB("", ""); err == nil {
+		t.Errorf("expected error when unpacking to or from an empty string")
+	}
+
+	td, err := ioutil.TempDir("", "kepubify-test")
+	if err != nil {
+		panic(err)
+	}
+	defer os.RemoveAll(td)
+
+	oepub := "test.epub"
+	if err := UnpackEPUB(oepub, td); err == nil {
+		t.Errorf("expected error when unpacking to existing dir")
+	}
+
+	dir := filepath.Join(td, "epub")
+	if err := UnpackEPUB(oepub, dir); err != nil {
+		t.Fatalf("unexpected error unpacking epub: %v", err)
+	}
+
+	for _, f := range []string{
+		"/",
+		"/mimetype",
+		"/OEBPS/",
+		"/OEBPS/text2.xhtml",
+		"/OEBPS/cover.jpg",
+		"/OEBPS/toc.ncx",
+		"/OEBPS/style.css",
+		"/OEBPS/text1.xhtml",
+		"/OEBPS/nav.xhtml",
+		"/OEBPS/cover.xhtml",
+		"/OEBPS/content.opf",
+		"/META-INF/",
+		"/META-INF/container.xml",
+	} {
+		fi, err := os.Stat(filepath.Join(dir, filepath.FromSlash(f)))
+		if err != nil {
+			t.Fatalf("expected %s to exist in unpacked dir", f)
+		} else if fi.IsDir() != strings.HasSuffix(f, "/") {
+			t.Fatalf("wrong type (dir/file) for %s", f)
+		}
+	}
+
+	pepub := filepath.Join(td, "packed.epub")
+	if err := PackEPUB(dir, pepub); err != nil {
+		t.Fatalf("unexpected error packing epub: %v", err)
+	}
+
+	if err := PackEPUB(dir, pepub); err != nil {
+		t.Fatalf("unexpected error packing epub over existing file: %v", err)
+	}
+
+	if _, err := os.Stat(pepub); err != nil {
+		t.Fatalf("packed epub %s doesn't exist", pepub)
+	}
+
+	oh := zipHash(oepub)
+	nh := zipHash(pepub)
+
+	if oh != nh {
+		t.Fatalf("content hashes don't match")
+	}
+
+	zr, err := zip.OpenReader(pepub)
+	if err != nil {
+		t.Fatalf("error opening packed epub: %v", err)
+	}
+	defer zr.Close()
+
+	if zr.File[0].Name != "mimetype" {
+		t.Errorf("packed epub: first entry not mimetype")
+	}
+
+	if zr.File[0].Method != zip.Store {
+		t.Errorf("packed epub: first entry not uncompressed")
+	}
+
+	for _, f := range zr.File {
+		if strings.Contains(f.Name, `\`) {
+			t.Errorf("packed epub: wrong path separator (%#v)!", f.Name)
+		}
+		if strings.HasPrefix(f.Name, `/`) {
+			t.Errorf("packed epub: path starts with / (%#v)", f.Name)
+		}
+		if filepath.Clean(filepath.FromSlash(f.Name)) != filepath.FromSlash(f.Name) {
+			t.Errorf("packed epub: unclean path (%#v)", f.Name)
+		}
+	}
+}
+
+func zipHash(path string) string {
+	zr, err := zip.OpenReader(path)
+	if err != nil {
+		panic(err)
+	}
+	defer zr.Close()
+
+	sort.Slice(zr.File, func(i, j int) bool {
+		return zr.File[i].Name < zr.File[j].Name
+	})
+
+	ss := sha256.New()
+	for _, f := range zr.File {
+		if err := binary.Write(ss, binary.LittleEndian, f.CRC32); err != nil {
+			panic(err)
+		}
+	}
+
+	return hex.EncodeToString(ss.Sum(nil))
+}
