48ee277c25f44dafa77eedb8f396447c568120a7
Patrick Gaskin <patrick@pgaskin.net>
2020-01-16 22:57:40 -0500

    Updated kobotest script

--- kepub/kobotest/test.sh
+++ kepub/kobotest/test.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-find . -name '*.xhtml' | while read f; do
+
+shopt -s globstar
+for f in **/*.xhtml; do
     tput reset
     echo "$f"
-    cat $f | go run . || { echo "Press enter to continue"; read tmp; }
+    cat "$f" | go run . || { echo "Press enter to continue"; read tmp; }
     tput reset
 done
